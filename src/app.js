b45dd3edf3833d96b7c7eb5fdb312de65bc6d227
karoljaw <karoljaworek04@gmail.com>
2018-04-25 09:26:52 +0200

    submit post

--- src/app.js
+++ src/app.js
@@ -3,8 +3,29 @@ import { ui } from "./ui";
 
 document.addEventListener('DOMContentLoaded', getPosts);
 
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
 function getPosts() {
     http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
+
+function submitPost() {
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+
+    const data = {
+        title,
+        body
+    }
+
+    //Create Post
+    http.post('http://localhost:3000/posts', data)
+    .then(data => {
+        ui.showAlert('Post adedd', 'alert alert-success');
+        ui.clearFields();
+        getPosts();
+    })
     .catch(err => console.log(err));
 }
